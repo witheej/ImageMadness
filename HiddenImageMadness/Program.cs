d80938b0030a9c19f7ebd10bf12cc3edc12ef7b3
Joshua Withee <jwithee@gmail.com>
2020-09-18 15:49:34 -0400

    Initial

---
+++ HiddenImageMadness/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace HiddenImageMadness
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Image img = System.Drawing.Image.FromFile("Papio-hamadryas-head.jpg");
+      float aspectRatio = (float)img.Height / (float)img.Width;
+      Image thumbnail = img.GetThumbnailImage(256, (int)(256 * aspectRatio), null, IntPtr.Zero);
+
+      string filepath = Path.GetTempFileName() + ".jpg";
+      Console.WriteLine($"Saving thumbnail to {filepath}");
+      thumbnail.Save(Path.GetTempFileName() + ".jpg");
+
+      Console.WriteLine("Press any key to exit...");
+      Console.ReadKey();
+    }
+  }
+}
